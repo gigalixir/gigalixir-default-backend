f5a79e1af5de343773c94ed521046ac9a4115436
Jesse <jesse@gigalixir.com>
2018-08-14 15:28:55 -0700

    Following gigalixir getting started steps. Seems to work.

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule GigalixirDefaultBackend.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
+      {:distillery, github: "bitwalker/distillery"},
       {:cowboy, "~> 1.0"}
     ]
   end
